280bf5ff46af55f67a949d581abe44b2440edcf3
Yohai Reshef <yohai.reshef@gmail.com>
2024-12-15 15:55:12 +0200

    Initial Commit

---
+++ Appeteasers/AppeteasersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppeteasersApp.swift
+//  Appeteasers
+//
+//  Created by Yohai on 15/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppeteasersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
