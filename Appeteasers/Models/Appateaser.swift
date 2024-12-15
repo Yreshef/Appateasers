431478096b3169bca1c2b3e7b83b73aaa423e89a
Yohai Reshef <yohai.reshef@gmail.com>
2024-12-15 16:21:37 +0200

    add model

---
+++ Appeteasers/Models/Appateaser.swift
@@ -0,0 +1,19 @@
+//
+//  Appateaser.swift
+//  Appeteasers
+//
+//  Created by Yohai on 15/12/2024.
+//
+
+import Foundation
+
+struct Appateaser: Decodable {
+    var imageURL: String
+    var name: String
+    var calories: Int
+    var protein: Int
+    var description: String
+    var price: Int
+    var carbs: Int
+}
+ 
